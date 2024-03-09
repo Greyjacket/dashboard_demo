9cd0f262ec252982ac9f8074df585d266d577461
greyjacket <alex@syncurity.net>
2024-03-09 18:17:13 -0500

    links added

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -8,24 +8,42 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+const links = [
+    {
+      name: 'Home',
+      href: '/',
+      icon: HomeIcon,
+    },
+    {
+      name: 'Users',
+      href: '/users',
+      icon: UserGroupIcon,
+    },
+    {
+      name: 'Documents',
+      href: '/documents',
+      icon: DocumentDuplicateIcon,
+    },
+  ];
+  
    
-  export default function NavLinks() {
-    const pathname = usePathname();
-    return (
-      <>
-        {links.map((link) => {
-          const LinkIcon = link.icon;
-          return (
-            <Link
-              key={link.name}
-              href={link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-            >
-              <LinkIcon className="w-6" />
-              <p className="hidden md:block">{link.name}</p>
-            </Link>
-          );
-        })}
-      </>
-    );
-  }
+export default function NavLinks() {
+const pathname = usePathname();
+return (
+    <>
+    {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+        <Link
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+        >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+        </Link>
+        );
+    })}
+    </>
+);
+}
